432aaeff5e3c532910ed9f70d8720a9c23b662cf
zSteme <61259296+zSteme@users.noreply.github.com>
2023-06-29 21:00:20 -0300

    Add files via upload
    Adicionado arquivos do projeto

---
+++ JavaWeb/src/main/java/com/horarioweb/Calculadora.java
@@ -0,0 +1,13 @@
+package com.horarioweb;
+
+public class Calculadora {
+
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+}
+
